b29f8e306608a2757127a74cb1e2b1c49176f794
ferleon82 <masilfer82@gmail.com>
2022-11-11 21:18:18 -0500

    sfd

--- proyecto_demo/app_productos/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-#from suit.apps import DjangoSuitConfig
-#from suit.menu import ParentItem, ChildItem
-
-#class SuitConfig(DjangoSuitConfig):
-#    layout = 'horizontal'
-    
-class AppProductosConfig(AppConfig):
-    name = 'app_productos'
